e15f43ea720ef586cd9d8e631111b73e934fdd14
manuelAW99 <mvilasvaliente@gmail.com>
2021-05-31 22:55:08 -0400

    tabla de reservas

--- TravelWithUsApp/src/app/components/reserve/reserve.component.ts
+++ TravelWithUsApp/src/app/components/reserve/reserve.component.ts
@@ -1,22 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-
-interface Agency {
-  value: string;
-  viewValue: string;
-  price: number;
-}
-
-interface Hotel {
-  value: string;
-  viewValue: string;
-  price: number;
-}
-
-interface Company {
-  value: string;
-  viewValue: string;
-  price: number;
-}
+import {Agencia} from "../table-agencies/agencies";
+import {Turista} from "../table-tourist/tourist";
+import {Oferta} from "../table-offers/offers";
 
 /**
  * @title Basic select with initial value and no form
@@ -27,48 +12,27 @@ interface Company {
   styleUrls: ['reserve.component.scss']
 })
 export class ReserveComponent {
-  agencies: Agency[] = [
-    {value: 'steak', viewValue: 'Steak', price: 111},
-    {value: 'pizza', viewValue: 'Pizza', price: 101},
-    {value: 'tacos', viewValue: 'Tacos', price: 122}
-  ];
-  hotels: Hotel[] = [
-    {value: 'ford', viewValue: 'Ford', price: 11},
-    {value: 'chevrolet', viewValue: 'Chevrolet', price: 12},
-    {value: 'dodge', viewValue: 'Dodge', price: 15}
-  ];
-  companies: Company[] = [
-    {value: 'ford', viewValue: 'Ford', price: 1},
-    {value: 'chevrolet', viewValue: 'Chevrolet', price:2},
-    {value: 'dodge', viewValue: 'Dodge', price:3}
-  ];
-  sCom = this.companies[2];
-  sHot = this.hotels[0];
-  sAge = this.agencies[0];
-  selectedCom = this.sCom.value;
-  selectedHot = this.sHot.value;
-  selectedAge = this.sAge.value;
-  reservedPrice: number = this.sCom.price + this.sHot.price + this.sAge.price;
+  agencies: Agencia[] = [];
+  offers: Oferta[] = [];
+  tourist: Turista[] = [];
+  selectedOff: string = '';
+  selectedTou: string = '';
+  selectedAge: string = '';
+  reservedPrice: number = 0;
 
   selectAge(event: Event) {
     this.selectedAge = (event.target as HTMLSelectElement).value;
   }
-  selectHot(event: Event) {
-    this.selectedHot = (event.target as HTMLSelectElement).value;
+  selectTou(event: Event) {
+    this.selectedTou = (event.target as HTMLSelectElement).value;
   }
-  selectCom(event: Event) {
-    this.selectedCom = (event.target as HTMLSelectElement).value;
+  selectOff(event: Event) {
+    this.selectedOff = (event.target as HTMLSelectElement).value;
   }
   price(){
     this.reservedPrice = 0;
-    for(let comp of this.companies){
-      if (comp.value == this.selectedCom) this.reservedPrice+= comp.price;
-    }
-    for(let hot of this.hotels){
-      if (hot.value == this.selectedHot) this.reservedPrice+= hot.price;
-    }
-    for(let age of this.agencies){
-      if (age.value == this.selectedAge) this.reservedPrice+= age.price;
+    for(let off of this.offers){
+      if (off.nombre == this.selectedOff) this.reservedPrice+= off.precio;
     }
   }
 }
--- TravelWithUsApp/src/app/components/table-offers/offers.ts
+++ TravelWithUsApp/src/app/components/table-offers/offers.ts
@@ -2,9 +2,9 @@
   ofertaID: number;
   nombre: string;
   descripcion: string;
-  precio: string;
+  precio: number;
   hotel: string;
-  constructor(id: number, nombre: string, descripcion: string, precio: string, hotel: string) {
+  constructor(id: number, nombre: string, descripcion: string, precio: number, hotel: string) {
     this.ofertaID = id;
     this.nombre = nombre;
     this.descripcion = descripcion;
