b7c725c64e45088226147d4e95a73a3ea1421bae
VictorLantigua <v.lantigua94@gmail.com>
2021-06-01 03:55:42 -0400

    Add some queries

--- TravelWithUsContextLib/Repositories/RequestsRepository.cs
+++ TravelWithUsContextLib/Repositories/RequestsRepository.cs
@@ -16,16 +16,19 @@ namespace TravelWithUs.DBContext.Repositories
 
         public async Task<IEnumerable<GananciaAgencia>> GetExpectedProfitAsync()
         {
-            // ReservaExcursionRepository reRepo = new ReservaExcursionRepository(this.dbContext);
-            // ReservaIndividualRepository riRepo = new ReservaIndividualRepository(this.dbContext);
-            // ReservaPaqueteRepository rpRepo = new ReservaPaqueteRepository(this.dbContext);
+            AgenciaRepository agenciaRepo = new AgenciaRepository(this.dbContext);
+            var agencias = await agenciaRepo.RetrieveAllAsync();
+            var query = agencias.Select(a => new GananciaAgencia(
+                a.Nombre,
+                a.ReservasExcursiones.Sum(re => re.Excursion.Precio)
+                + a.ReservasIndividuales.Sum(ri => ri.Precio)
+                + a.ReservasPaquetes.Sum(rp => rp.Precio)
+                , a.ReservasExcursiones.Count
+                + a.ReservasIndividuales.Count
+                + a.ReservasPaquetes.Count)
+                );
 
-            // var reAll = await reRepo.RetrieveAllAsync();
-            // var riAll = await riRepo.RetrieveAllAsync();
-            // var rpAll = await rpRepo.RetrieveAllAsync();
-            // var reAgencia = reAll.GroupBy(re => re.Excursion.Precio)
-            //                 .Where(re => )
-            throw new System.NotImplementedException();
+            return query;
         }
 
         public Task<IEnumerable<ExcursionExtendida>> GetExtendedExcursion()
@@ -54,14 +57,28 @@ namespace TravelWithUs.DBContext.Repositories
             throw new System.NotImplementedException();
         }
 
-        public Task<PaqueteSobreMedia> GetPackagesOverMean()
+        public async Task<PaqueteSobreMedia> GetPackagesOverMean()
         {
-            throw new System.NotImplementedException();
+            PaqueteRepository paqueteRepo = new PaqueteRepository(this.dbContext);
+            var paquetes = await paqueteRepo.RetrieveAllAsync();
+            decimal media = paquetes.Average(p => p.Precio);
+            var query = new PaqueteSobreMedia(paquetes.Where(
+                p => p.Precio > media
+            ));
+
+            return query;
         }
 
-        public Task<IEnumerable<TuristaIndividualRepitente>> GetRepetitiveTouristAsync()
+        public async Task<IEnumerable<TuristaIndividualRepitente>> GetRepetitiveTouristAsync()
         {
-            throw new System.NotImplementedException();
+            TuristaRepository turistaRepo = new TuristaRepository(this.dbContext);
+            var turistas = await turistaRepo.RetrieveAllAsync();
+            var query = turistas.Where(t => t.ReservasIndividuales.Count > 1)
+            .Select(t => new TuristaIndividualRepitente(
+                t.Nombre, t.Email, t.ReservasIndividuales.Count
+            ));
+
+            return query;
         }
     }
 }
