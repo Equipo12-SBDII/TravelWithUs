f7f4fdceb5cf3b039829a60fa3be3abfadaf15b3
manuelAW99 <mvilasvaliente@gmail.com>
2021-06-05 22:47:24 -0400

    borrando lo innecesario

--- TravelWithUsApp/src/app/app.module.ts
+++ TravelWithUsApp/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { routing } from "./app-routing.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
@@ -51,8 +49,6 @@ import { TableReservesComponent } from './components/table-reserves/table-reserv
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     HomeComponent,
     ReserveComponent,
     NavbarComponent,
